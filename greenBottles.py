a978d33c3f42dc132bd8602571ff7684868ab245
Josiah Faulkner-Harrington <jfaulknerharrington6996@sohamcollege.org.uk>
2014-05-07 09:43:40 +0100

    Update greenBottles.py
    completed and added time pauses so it isn't really quick and unable to read

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,12 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+
+bottles = 10
+for x in range(bottles,0,-1):
+    print("{0} green bottle hanging on the wall.".format(x))
+    time.sleep(1)
+    print("{0} green bottles hanging on the wall.".format(x))
+    time.sleep(1)
+    print("And if 1 green bottle should acidentally fall,")
+    time.sleep(1)
+    print("There'll be {0} green bottles hanging on the wall.\n".format(x-1))
+    time.sleep(1)
